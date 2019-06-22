d3676d0b16c8e9c1ba735e8c5f37d137d6f0f71b
Griko Nibras <grikomsn@gmail.com>
2019-06-23 01:00:42 +0700

    Redirect from Bulma Expo to V2

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -3,11 +3,18 @@ import React from 'react'
 import wrapPageElementWithTransition from './src/helpers/wrapPageElement'
 import AppProvider from './src/store/provider'
 
-// React Context in Browser
+// https://www.gatsbyjs.org/docs/browser-apis/#onClientEntry
+export const onClientEntry = () => {
+  if (/https:\/\/bulma\.io\/expo/.test(document.referrer)) {
+    window.location.href = 'https://v2.griko.id'
+  }
+}
+
+// https://www.gatsbyjs.org/docs/browser-apis/#wrapPageElement
+export const wrapPageElement = wrapPageElementWithTransition
+
+// https://www.gatsbyjs.org/docs/browser-apis/#wrapRootElement
 // eslint-disable-next-line react/prop-types
 export const wrapRootElement = ({ element }) => {
   return <AppProvider>{element}</AppProvider>
 }
-
-// Page Transitions
-export const wrapPageElement = wrapPageElementWithTransition
