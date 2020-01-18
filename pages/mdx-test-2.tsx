9d1bcfff964de2809e563a0172a4bc06568884c3
Shu Uesugi <shu@chibicode.com>
2020-01-17 16:21:55 -0800

    Simplify

--- pages/mdx-test-2.tsx
+++ pages/mdx-test-2.tsx
@@ -1,17 +1,11 @@
-import { NextSeo } from 'next-seo'
 import React from 'react'
-
-import Content from '../components/content'
 import Main from '../components/main'
 import Test2 from '../contents/test-2.mdx'
 
 function Contact() {
   return (
     <Main>
-      <NextSeo title="Test" />
-      <Content>
-        <Test2 />
-      </Content>
+      <Test2 />
     </Main>
   )
 }
--- pages/mdx-test.tsx
+++ pages/mdx-test.tsx
@@ -1,17 +1,11 @@
-import { NextSeo } from 'next-seo'
 import React from 'react'
-
-import Content from '../components/content'
 import Main from '../components/main'
 import Test from '../contents/test.mdx'
 
 function Contact() {
   return (
     <Main>
-      <NextSeo title="Test" />
-      <Content>
-        <Test />
-      </Content>
+      <Test />
     </Main>
   )
 }
