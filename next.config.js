d8ebc59314762a74b0585086b29d7c5321bcd282
Griko Nibras <git@griko.id>
2020-01-23 04:07:37 +0700

    Revert "Remove next.config envs"
    This reverts commit 816b1b86fb56e44ac7846cb72996324768e0e92c.

--- next.config.js
+++ next.config.js
@@ -11,6 +11,10 @@ module.exports = withPlugins(
     [require('next-optimized-images')],
   ],
   {
+    env: {
+      CONTENTFUL_SPACE: process.env.CONTENTFUL_SPACE,
+      CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    },
     target: 'serverless',
   }
 )
