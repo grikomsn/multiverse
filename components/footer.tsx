28ff08f7f2bd1be00f2e22379be139389f546931
Griko Nibras <git@griko.id>
2020-01-23 05:05:10 +0700

    Update footer and source license

--- components/footer.tsx
+++ components/footer.tsx
@@ -5,11 +5,13 @@ import A from './a'
 function Footer() {
   return (
     <footer className="p-4 text-center text-gray-400 text-xs transition-colors">
-      &copy; Griko Nibras {new Date().getFullYear()}. Made with{' '}
-      <A href="https://nextjs.org" bold children="Next.js" />,{' '}
-      <A href="https://tailwindcss.com" bold children="Tailwind CSS" />, and{' '}
-      <A href="https://www.framer.com/motion" bold children="Framer Motion" />.
-      Hosted on <A href="https://zeit.co/now" bold children="Zeit Now" />.
+      Made with <A href="https://nextjs.org" bold children="Next.js" />,{' '}
+      <A href="https://tailwindcss.com" bold children="Tailwind CSS" />,{' '}
+      <A href="https://www.framer.com/motion" bold children="Framer Motion" />,{' '}
+      and <A href="https://contentful.com" bold children="Contentful" />. Hosted
+      on <A href="https://zeit.co/now" bold children="Zeit Now" />. Source
+      licensed under the{' '}
+      <A href="https://griko.dev/landing-page" bold children="MIT License" />.
     </footer>
   )
 }
