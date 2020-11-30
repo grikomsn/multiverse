1e7393f459b3cf28d5e3f26f5adf4cd2c5f3a1c4
Griko Nibras <git@griko.id>
2020-11-30 12:16:17 +0700

    Add me.jpg rewrite

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
+const { contentful } = require("./src/cms");
+
 module.exports = {
   reactStrictMode: true,
   async rewrites() {
+    const data = await contentful().request(/* GraphQL */ `
+      {
+        aboutPage: aboutPageCollection(limit: 1) {
+          items {
+            avatar {
+              url(transform: { format: JPG, height: 1280, width: 1280 })
+            }
+          }
+        }
+      }
+    `);
     return [
+      {
+        source: "/me.jpg",
+        destination: data.aboutPage.items[0].avatar.url,
+      },
       {
         source: "/social-alt.png",
         destination: "/api/social-image",
