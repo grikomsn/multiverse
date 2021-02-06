92327ca290fdf090075b725eb60b365ccb4da440
Griko Nibras <git@griko.id>
2021-02-06 22:21:56 +0700

    feat: update configs

--- .eslintrc.js
+++ .eslintrc.js
@@ -6,22 +6,67 @@ module.exports = {
     "kentcdodds/react",
     "kentcdodds/jsx-a11y",
   ],
-  plugins: ["prettier"],
+
+  plugins: ["prettier", "simple-import-sort"],
+
   rules: {
+    // import rules
     "import/extensions": [
       "warn",
       "never",
       {
         css: "ignorePackages",
-        json: "ignorePackages",
+        graphql: "ignorePackages",
       },
     ],
-    "require-await": "off",
+    "import/newline-after-import": "warn",
+    "import/order": "off",
+    "sort-imports": "off",
 
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-unnecessary-condition": "off",
-    "@typescript-eslint/no-unsafe-assignment": "off",
-    "@typescript-eslint/require-await": "off",
-    "@typescript-eslint/restrict-template-expressions": "off",
+    // simple import rules
+    "simple-import-sort/exports": "warn",
+    "simple-import-sort/imports": [
+      "warn",
+      {
+        groups: [
+          ["^.+\\.s?css$"],
+          ["^\\u0000"],
+          ["^react$"],
+          ["^@/"],
+          ["^\\.\\.(?!/?$)", "^\\.\\./?$"],
+          ["^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        ],
+      },
+    ],
+
+    // react rules
+    "react/jsx-sort-props": [
+      "warn",
+      {
+        reservedFirst: ["key"],
+      },
+    ],
+
+    // various rules
+    "max-lines-per-function": "off",
+    "no-void": "off",
+    "require-await": "off",
   },
+
+  overrides: [
+    {
+      files: ["*.ts", "*.tsx"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        project: "./tsconfig.json",
+      },
+      rules: {
+        "@typescript-eslint/no-explicit-any": "warn",
+        "@typescript-eslint/no-unnecessary-condition": "off",
+        "@typescript-eslint/no-unsafe-assignment": "off",
+        "@typescript-eslint/require-await": "off",
+        "@typescript-eslint/restrict-template-expressions": "off",
+      },
+    },
+  ],
 };
