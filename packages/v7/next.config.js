5a4816c1b164ecdd325a0f6e0e962d3eda57bb3f
Griko Nibras <git@griko.id>
2021-05-04 09:03:16 +0700

    feat(v7): add cache control header

--- packages/v7/next.config.js
+++ packages/v7/next.config.js
@@ -6,6 +6,21 @@ module.exports = {
     webpack5: true,
   },
 
+  // https://nextjs.org/docs/api-reference/next.config.js/headers
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "s-maxage=1, stale-while-revalidate",
+          },
+        ],
+      },
+    ];
+  },
+
   // https://nextjs.org/docs/basic-features/image-optimization#domains
   images: {
     domains: [
