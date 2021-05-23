c142e989986abd0f9f4db453de149de61f2dd518
Griko Nibras <git@griko.id>
2021-05-23 12:50:05 +0700

    fix(v7): use next link

--- packages/v7/components/post-item/index.tsx
+++ packages/v7/components/post-item/index.tsx
@@ -7,7 +7,6 @@ import {
   Box,
   BoxProps,
   Heading,
-  Link,
   Stack,
   Tag,
   Text,
@@ -15,6 +14,7 @@ import {
   WrapItem,
 } from "@chakra-ui/react";
 import format from "date-fns/format";
+import Link from "next/link";
 
 interface PostItemProps extends BoxProps {
   post: PostMetaFieldsFragment;
