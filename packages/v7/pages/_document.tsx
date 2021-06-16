8002674060644c6e9d6c1e99c28806dfff2e9321
Griko Nibras <git@griko.id>
2021-06-16 15:17:51 +0700

    chore(v7): update _document

--- packages/v7/pages/_document.tsx
+++ packages/v7/pages/_document.tsx
@@ -3,11 +3,23 @@ import * as React from "react";
 import analyticsConfig from "~config/analytics";
 
 import { ColorModeScript } from "@chakra-ui/react";
-import NextDocument, { DocumentContext, Head, Html, Main, NextScript } from "next/document";
+import Document, {
+  DocumentContext,
+  Head,
+  Html,
+  Main,
+  NextScript,
+} from "next/document";
+import Script from "next/script";
 
-export default class Document extends NextDocument {
+export default class CustomDocument extends Document {
+  fonts = [
+    "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
+    "https://fonts.googleapis.com/css2?family=Manrope:wght@700;800&display=swap",
+  ];
+
   static async getInitialProps(ctx: DocumentContext) {
-    const initialProps = await NextDocument.getInitialProps(ctx);
+    const initialProps = await Document.getInitialProps(ctx);
     return { ...initialProps };
   }
 
@@ -19,8 +31,9 @@ export default class Document extends NextDocument {
           <meta content="ie=edge" httpEquiv="X-UA-Compatible" />
 
           <link href="https://fonts.gstatic.com" rel="preconnect" />
-          <link href="https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
-          <link href="https://fonts.googleapis.com/css2?family=Manrope:wght@700;800&display=swap" rel="stylesheet" />
+          {this.fonts.map((font) => (
+            <link key={font} href={font} rel="stylesheet" />
+          ))}
         </Head>
 
         <body>
@@ -28,7 +41,10 @@ export default class Document extends NextDocument {
           <Main />
           <NextScript />
 
-          <script data-cf-beacon={`{"token": "${analyticsConfig.cloudflareBeaconToken}"}`} defer src="https://static.cloudflareinsights.com/beacon.min.js" />
+          <Script
+            data-cf-beacon={`{"token": "${analyticsConfig.cloudflareBeaconToken}"}`}
+            src="https://static.cloudflareinsights.com/beacon.min.js"
+          />
         </body>
       </Html>
     );
