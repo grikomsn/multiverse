0a7d794eb9c1ed409fcc40cc5e3b8fcfab3a0fd0
Griko Nibras <git@griko.id>
2021-07-03 06:50:25 +0700

    refactor(v7): add security headers and simplify next config

--- packages/v7/next.config.js
+++ packages/v7/next.config.js
@@ -1,7 +1,16 @@
 const path = require("path");
 
-const headers = require("./config/headers");
-const redirects = require("./config/redirects");
+// https://github.com/leerob/leerob.io/blob/9adc510cbfb3da88c3b0ad15632eb876ca91b607/next.config.js#L39-L49
+const csp = `
+  default-src 'self';
+  script-src 'self' 'unsafe-eval' 'unsafe-inline';
+  child-src *.twitter.com;
+  style-src 'self' 'unsafe-inline' fonts.googleapis.com;
+  img-src * blob: data:;
+  media-src 'none';
+  connect-src *;
+  font-src 'self' fonts.gstatic.com;
+`;
 
 /** @type {import("next/dist/next-server/server/config-shared").NextConfig} */
 module.exports = {
@@ -15,7 +24,65 @@ module.exports = {
   },
 
   // https://nextjs.org/docs/api-reference/next.config.js/headers
-  headers,
+  // https://github.com/leerob/leerob.io/blob/9adc510cbfb3da88c3b0ad15632eb876ca91b607/next.config.js#L51-L88
+  async headers() {
+    return [
+      {
+        source: "/(.*)",
+        headers: [
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
+          {
+            key: "Cache-Control",
+            value: "public, s-maxage=1, stale-while-revalidate=59",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/CSP
+          {
+            key: "Content-Security-Policy",
+            value: csp.replace(/\n/g, ""),
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Feature-Policy
+          // Opt-out of Google FLoC: https://amifloced.org/
+          {
+            key: "Permissions-Policy",
+            value:
+              "camera=(), microphone=(), geolocation=(), interest-cohort=()",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Referrer-Policy
+          {
+            key: "Referrer-Policy",
+            value: "origin-when-cross-origin",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Strict-Transport-Security
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Frame-Options
+          {
+            key: "X-Frame-Options",
+            value: "DENY",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-Content-Type-Options
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+
+          // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/X-DNS-Prefetch-Control
+          {
+            key: "X-DNS-Prefetch-Control",
+            value: "on",
+          },
+        ],
+      },
+    ];
+  },
 
   // https://nextjs.org/docs/basic-features/image-optimization#domains
   images: {
@@ -30,7 +97,15 @@ module.exports = {
   reactStrictMode: true,
 
   // https://nextjs.org/docs/api-reference/next.config.js/redirects
-  redirects,
+  async redirects() {
+    return [
+      // {
+      //   source: "/blog/:path*",
+      //   destination: "/blog",
+      //   permanent: false,
+      // },
+    ];
+  },
 
   // https://nextjs.org/docs/api-reference/next.config.js/custom-webpack-config
   /**
