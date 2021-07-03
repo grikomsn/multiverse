427863f1eaa5a6ae199d49ca673f7cadedcf161b
Griko Nibras <git@griko.id>
2021-07-03 07:11:56 +0700

    feat(v7): add cors headers

--- packages/v7/next.config.js
+++ packages/v7/next.config.js
@@ -30,6 +30,24 @@ module.exports = {
       {
         source: "/(.*)",
         headers: [
+          // https://vercel.com/support/articles/how-to-enable-cors#enabling-cors-in-a-next.js-app
+          {
+            key: "Access-Control-Allow-Credentials",
+            value: "true",
+          },
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Access-Control-Allow-Methods",
+            value: "GET,OPTIONS,PATCH,DELETE,POST,PUT",
+          },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: `X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version`,
+          },
+
           // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Cache-Control
           {
             key: "Cache-Control",
