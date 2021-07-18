c5de66ba3f7d22049367974085f3b1dd5a7ce4d9
Griko Nibras <git@griko.id>
2021-07-18 23:32:36 +0700

    chore(v7): remove unused _document fonts prop

--- packages/v7/pages/_document.tsx
+++ packages/v7/pages/_document.tsx
@@ -16,12 +16,8 @@ import Script from "next/script";
 
 // eslint-disable-next-line @typescript-eslint/unbound-method
 const { extractCritical } = createEmotionServer(emotionCache);
-export default class CustomDocument extends Document {
-  fonts = [
-    "https://fonts.googleapis.com/css2?family=Archivo:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap",
-    "https://fonts.googleapis.com/css2?family=Manrope:wght@700;800&display=swap",
-  ];
 
+export default class CustomDocument extends Document {
   static async getInitialProps(ctx: DocumentContext) {
     const initialProps = await Document.getInitialProps(ctx);
     const styles = extractCritical(initialProps.html);
