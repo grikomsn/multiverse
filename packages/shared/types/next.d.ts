fc1af367cff92414802ca8f1e9e52f9eb7b52aab
Griko Nibras <git@griko.id>
2021-07-25 20:22:33 +0700

    feat(shared): add custom next types

--- packages/shared/types/next.d.ts
+++ packages/shared/types/next.d.ts
@@ -1,5 +1,25 @@
-import { NextApiRequest as BaseNextApiRequest } from "next";
+import {
+  NextApiRequest as BaseNextApiRequest,
+  NextComponentType as BaseNextComponentType,
+  NextPage as BaseNextPage,
+  NextPageContext,
+} from "next";
 
 export type NextApiRequest<
   T extends Record<string, unknown> = { [k: string]: unknown },
 > = BaseNextApiRequest & T;
+
+export type NextComponentType<IP = unknown, P = {}> = BaseNextComponentType<
+  NextPageContext,
+  IP,
+  P
+> &
+  NextPageProps;
+
+export type NextPage<P = {}, IP = P> = BaseNextPage<P, IP> & NextPageProps;
+
+export type NextPageProps = {
+  layout?: LayoutType;
+};
+
+export type LayoutType = "default" | "none";
