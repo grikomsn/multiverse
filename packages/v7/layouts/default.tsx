062ca6c0b499d9bb20e0d800f7d1ada8b6ac34ea
Griko Nibras <git@griko.id>
2021-08-22 18:32:12 +0700

    [v7] chore(layout): remove page transition

--- packages/v7/layouts/default.tsx
+++ packages/v7/layouts/default.tsx
@@ -3,7 +3,6 @@ import * as React from "react";
 import Footer from "~components/footer";
 import KeybindsCheatsheet from "~components/keybinds-cheatsheet/lazy";
 import MobileDrawer from "~components/mobile-drawer/lazy";
-import MotionBox from "~components/motion/box";
 import Navbar from "~components/navbar";
 import siteConfig from "~config/site";
 import meta from "~generated/meta.json";
@@ -11,31 +10,19 @@ import useKeybinds from "~hooks/use-keybinds";
 import useNProgress from "~hooks/use-nprogress";
 import { useCheatsheetSyncSetup } from "~store/global";
 
-import { Flex } from "@chakra-ui/react";
-import { AnimatePresence } from "framer-motion";
+import { Box, Flex } from "@chakra-ui/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { DefaultSeo, SocialProfileJsonLd } from "next-seo";
 import { OpenGraphImages } from "next-seo/lib/types";
 
-const PAGE_TRANSITION_PROPS = {
-  animate: "enter",
-  exit: "exit",
-  initial: "initial",
-  variants: {
-    initial: { opacity: 0 },
-    enter: { duration: 0.2, opacity: 1 },
-    exit: { duration: 0.1, opacity: 0 },
-  },
-};
-
-const Effects: React.FC = () => {
+function Effects() {
   useCheatsheetSyncSetup();
   useKeybinds();
   useNProgress();
 
   return null;
-};
+}
 
 const DefaultLayout: React.FC = (props) => {
   const { children } = props;
@@ -76,15 +63,9 @@ const DefaultLayout: React.FC = (props) => {
 
       <Flex flexDir="column" minH="100vh">
         <Navbar />
-        <AnimatePresence exitBeforeEnter>
-          <MotionBox
-            key={router.asPath}
-            flexGrow={1}
-            {...PAGE_TRANSITION_PROPS}
-          >
-            {children}
-          </MotionBox>
-        </AnimatePresence>
+        <Box key={router.asPath} flexGrow={1}>
+          {children}
+        </Box>
         <Footer />
         <MobileDrawer />
         <KeybindsCheatsheet />
