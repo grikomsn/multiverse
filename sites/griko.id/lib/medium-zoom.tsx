c572daf43cb2ffca62be4ea8471f3870c4e5e27f
Griko Nibras <git@griko.id>
2022-01-03 00:02:22 +0700

    [griko.id] feat: update medium-zoom
    Signed-off-by: Griko Nibras <git@griko.id>

--- sites/griko.id/lib/medium-zoom.tsx
+++ sites/griko.id/lib/medium-zoom.tsx
@@ -1,12 +1,14 @@
 import * as React from "react";
 
+import { ZoomSelector } from "medium-zoom";
+
 export function RegisterMediumZoom() {
   React.useEffect(() => {
     if (!window.mediumZoom) {
       void import("medium-zoom").then((mod) => {
         window.mediumZoom = mod.default({
           background: "",
-          margin: 64,
+          margin: 16,
         });
       });
     }
@@ -15,18 +17,20 @@ export function RegisterMediumZoom() {
   return null;
 }
 
-export function useMediumZoom() {
-  const [attach, setAttach] = React.useState<{ mz: ((ref: HTMLImageElement) => void) | null }>({
-    mz: null,
+export function useMediumZoom(deps: unknown[] = []) {
+  const [attach, setAttach] = React.useState<{ mz: (ref: unknown) => void }>({
+    mz: () => {},
   });
 
   React.useEffect(() => {
     setAttach({
       mz: (ref) => {
-        window.mediumZoom?.clone().attach(ref);
+        window.mediumZoom?.clone().attach(ref as ZoomSelector);
       },
     });
-  }, []);
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps);
 
   return attach?.mz;
 }
