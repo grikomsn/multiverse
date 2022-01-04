ee6f40357059326f8d1f070256625f06d471b824
Griko Nibras <git@griko.id>
2022-01-05 03:50:10 +0700

    [griko.id] feat: update frontmatter types
    Signed-off-by: Griko Nibras <git@griko.id>

--- sites/griko.id/lib/remark/types.ts
+++ sites/griko.id/lib/remark/types.ts
@@ -2,7 +2,9 @@ export type Frontmatter = {
   title: string;
   description: string;
   date: Date;
+  header?: boolean;
   lastUpdate?: Date;
+  [key: string]: unknown;
 };
 
 export type FrontmatterEntry = readonly [string, Frontmatter];
