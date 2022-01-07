0304db17c1eac9f15f3920836397cd7353bb980b
Griko Nibras <git@griko.id>
2022-01-07 16:44:35 +0700

    [griko.id] fix: improve seo hook types

--- sites/griko.id/utils/seo.tsx
+++ sites/griko.id/utils/seo.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 
 import { NextSeo, NextSeoProps } from "next-seo";
 
-export function useSeo<T extends NextSeoProps>(props: T) {
+export function useSeo<T>(props: T & NextSeoProps) {
   function Seo(extraProps: NextSeoProps) {
     return <NextSeo {...props} {...extraProps} />;
   }
