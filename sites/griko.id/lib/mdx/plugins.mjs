e61e21d3907de2f911768b378d9cd30d5b20ec68
Griko Nibras <git@griko.id>
2022-01-07 15:10:07 +0700

    [griko.id] refactor: migrate to rehype-pretty-code

--- sites/griko.id/lib/mdx/plugins.mjs
+++ sites/griko.id/lib/mdx/plugins.mjs
@@ -1,7 +1,8 @@
-import remarkPrettyCode from "../remark/pretty-code.mjs";
+import { prettyCodeOptions } from "../rehype/pretty-code.mjs";
 import remarkTwemoji from "../remark/twemoji.mjs";
 
 import { nodeTypes } from "@mdx-js/mdx";
+import rehypePrettyCode from "rehype-pretty-code";
 import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug";
 import remarkFrontmatter from "remark-frontmatter";
@@ -12,6 +13,7 @@ import remarkToc from "remark-toc";
 /** @type {import("unified").PluggableList} */
 export const rehypePlugins = [
   [rehypeRaw, { passThrough: nodeTypes }],
+  [rehypePrettyCode, prettyCodeOptions],
   rehypeSlug,
   //
 ];
@@ -22,7 +24,6 @@ export const remarkPlugins = [
   [remarkMdxFrontmatter, { name: "frontmatter" }],
   remarkGfm,
   remarkToc,
-  remarkPrettyCode,
   remarkTwemoji,
   //
 ];
