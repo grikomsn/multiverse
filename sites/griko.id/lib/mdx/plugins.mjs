4aaca96e304d7adf175aa9f3e8ea700908b5749d
Griko Nibras <git@griko.id>
2022-01-08 00:27:21 +0700

    [griko.id] refactor: back to remark pretty code

--- sites/griko.id/lib/mdx/plugins.mjs
+++ sites/griko.id/lib/mdx/plugins.mjs
@@ -1,8 +1,7 @@
-import { prettyCodeOptions } from "../rehype/pretty-code.mjs";
+import remarkPrettyCode from "../remark/pretty-code.mjs";
 import remarkTwemoji from "../remark/twemoji.mjs";
 
 import { nodeTypes } from "@mdx-js/mdx";
-import rehypePrettyCode from "rehype-pretty-code";
 import rehypeRaw from "rehype-raw";
 import rehypeSlug from "rehype-slug";
 import remarkFrontmatter from "remark-frontmatter";
@@ -10,10 +9,13 @@ import remarkGfm from "remark-gfm";
 import { remarkMdxFrontmatter } from "remark-mdx-frontmatter";
 import remarkToc from "remark-toc";
 
+// import rehypePrettyCode from "rehype-pretty-code";
+// import remarkPrettyCode from "../remark/pretty-code.mjs";
+
 /** @type {import("unified").PluggableList} */
 export const rehypePlugins = [
   [rehypeRaw, { passThrough: nodeTypes }],
-  [rehypePrettyCode, prettyCodeOptions],
+  // [rehypePrettyCode, prettyCodeOptions],
   rehypeSlug,
   //
 ];
@@ -23,6 +25,7 @@ export const remarkPlugins = [
   remarkFrontmatter,
   [remarkMdxFrontmatter, { name: "frontmatter" }],
   remarkGfm,
+  remarkPrettyCode,
   remarkToc,
   remarkTwemoji,
   //
---
+++ sites/griko.id/lib/remark/pretty-code.mjs
@@ -0,0 +1,17 @@
+import theme from "../shiki-theme.cjs";
+
+import { createRemarkPlugin } from "@atomiks/mdx-pretty-code";
+
+const remarkPrettyCode = createRemarkPlugin({
+  onVisitHighlightedLine(node) {
+    node.classList.add("highlight-line");
+  },
+  onVisitHighlightedWord(node) {
+    node.classList.add("highlight-word");
+  },
+  shikiOptions: {
+    theme,
+  },
+});
+
+export default remarkPrettyCode;
