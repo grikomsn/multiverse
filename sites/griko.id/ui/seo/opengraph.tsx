49bf7294b163693edeb5bdaf497ffc161fad5cb7
Griko Nibras <git@griko.id>
2022-01-07 16:45:17 +0700

    [griko.id] fix: add preload tag to head

--- sites/griko.id/ui/seo/opengraph.tsx
+++ sites/griko.id/ui/seo/opengraph.tsx
@@ -3,6 +3,7 @@ import * as React from "react";
 import { createQueryParams, MainOpengraphQuery, Type } from "@/lib/opengraph";
 import getAbsoluteUrl from "@/utils/api";
 
+import Head from "next/head";
 import { useRouter } from "next/router";
 import { NextSeo } from "next-seo";
 
@@ -23,7 +24,9 @@ export default function OpenGraph({ type = "main", query = {} }: OpenGraphProps)
 
   return (
     <>
-      <link as="image" href={url} rel="preload" />
+      <Head>
+        <link as="image" href={url} rel="preload" />
+      </Head>
       <NextSeo openGraph={{ images: [{ url }] }} />
     </>
   );
