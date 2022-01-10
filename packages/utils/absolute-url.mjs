e8293c85484e3d20570cf30bd291785befaba443
Griko Nibras <git@griko.id>
2022-01-10 17:37:24 +0700

    [utils] feat: extract getAbsoluteUrl

---
+++ packages/utils/absolute-url.mjs
@@ -0,0 +1,32 @@
+// @ts-check
+
+/**
+ * @template T
+ * @param {T & import('http').IncomingMessage} req
+ * @param {string} defaultHost
+ */
+export function getAbsoluteUrl(req, defaultHost = "localhost:3000") {
+  let host = defaultHost;
+
+  if (req?.headers?.host) {
+    host = req.headers.host;
+  }
+  if (typeof window != "undefined") {
+    host = window.location.host;
+  }
+  if (typeof req?.headers["x-forwarded-host"] == "string") {
+    host = req.headers["x-forwarded-host"];
+  }
+
+  let protocol = /^localhost(:\d+)?$/.test(host) ? "http:" : "https:";
+
+  if (typeof req?.headers["x-forwarded-proto"] == "string") {
+    protocol = `${req.headers["x-forwarded-proto"]}:`;
+  }
+
+  return {
+    protocol,
+    host,
+    origin: `${protocol}//${host}`,
+  };
+}
--- sites/griko.id/utils/api.ts
+++ sites/griko.id/utils/api.ts
@@ -1,26 +1,10 @@
-import meta from "@/config/meta.json";
+import meta from "@/config/meta.cjs";
 
-import { NextApiRequest } from "next";
+import { getAbsoluteUrl as _getAbsoluteUrl } from "@packages/utils/absolute-url";
 
-export function getAbsoluteUrl(req?: NextApiRequest, localhostAddress = "localhost:3000") {
-  let host =
-    (req?.headers ? req.headers.host : typeof window == "undefined" ? undefined : window.location.host) ?? __DEV__
-      ? localhostAddress
-      : meta.domain;
-
-  let protocol = /^localhost(:\d+)?$/.test(host) ? "http:" : "https:";
-
-  if (req?.headers["x-forwarded-host"] && typeof req.headers["x-forwarded-host"] == "string") {
-    host = req.headers["x-forwarded-host"];
+export function getAbsoluteUrl<T>(req?: T & import("http").IncomingMessage) {
+  if (__PROD__) {
+    return _getAbsoluteUrl(req, meta.domain);
   }
-
-  if (req?.headers["x-forwarded-proto"] && typeof req.headers["x-forwarded-proto"] == "string") {
-    protocol = `${req.headers["x-forwarded-proto"]}:`;
-  }
-
-  return {
-    protocol,
-    host,
-    origin: `${protocol}//${host}`,
-  };
+  return _getAbsoluteUrl(req);
 }
