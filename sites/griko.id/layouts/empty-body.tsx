9d4197abedcafef23e5914b0bc6e372831533fbc
Griko Nibras <git@griko.id>
2022-01-18 05:50:03 +0700

    [griko.id] feat: add empty-body layout
    Signed-off-by: Griko Nibras <git@griko.id>

--- sites/griko.id/layouts/empty-body.tsx
+++ sites/griko.id/layouts/empty-body.tsx
@@ -1,6 +1,12 @@
 import * as React from "react";
 
+import { RegisterFathom } from "@/lib/fathom";
+import PageProgress from "@/ui/page-progress";
+
+import { DefaultAppSeo } from "./default";
+
 import Head from "next/head";
+import { Toaster } from "react-hot-toast";
 
 export default function EmptyLayout({ children }: React.PropsWithChildren<{}>) {
   return (
@@ -8,6 +14,12 @@ export default function EmptyLayout({ children }: React.PropsWithChildren<{}>) {
       <Head>
         <meta content="minimum-scale=1, initial-scale=1, width=device-width" name="viewport" />
       </Head>
+
+      <DefaultAppSeo />
+      <RegisterFathom />
+      <Toaster position="top-center" />
+
+      <PageProgress />
 
       {children}
     </>
--- sites/griko.id/layouts/index.ts
+++ sites/griko.id/layouts/index.ts
@@ -3,6 +3,7 @@ import dynamic from "next/dynamic";
 const layouts = {
   default: dynamic(() => import("./default")),
   empty: dynamic(() => import("./empty")),
+  "empty-body": dynamic(() => import("./empty-body")),
 };
 
 export type LayoutType = keyof typeof layouts;
