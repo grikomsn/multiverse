44257dc16948a95254f7a9152f7f555cd395239c
Griko Nibras <git@griko.id>
2022-01-22 02:38:46 +0700

    chore: remove bulma.griko.id

--- sites/bulma.griko.id/env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="@packages/types/vercel" />
-
-declare const __DEV__: boolean;
-declare const __PROD__: boolean;
--- sites/bulma.griko.id/pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import "bulma/css/bulma.css";
-
-import * as React from "react";
-
-import { AppProps } from "next/app";
-
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
-}
