be60e9a1863e168bc7df903314f2779d7580edd7
Griko Nibras <git@griko.id>
2022-04-27 02:29:28 +0700

    feat(store): handle cmd k

--- website/lib/fathom.ts
+++ website/lib/fathom.ts
@@ -1,25 +1,3 @@
-import metadata from "config/metadata";
-import * as Fathom from "fathom-client";
-import { useRouter } from "next/router";
-import { useEffect } from "react";
+export * as Fathom from "fathom-client";
 
-export function useFathom() {
-  const router = useRouter();
-
-  useEffect(() => {
-    const track = () => Fathom.trackPageview();
-    if (__PROD__) {
-      Fathom.load(metadata.fathom.id, {
-        includedDomains: [metadata.domain],
-        url: metadata.fathom.scriptUrl,
-      });
-      router.events.on("routeChangeComplete", track);
-      return () => {
-        router.events.off("routeChangeComplete", track);
-      };
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  return null;
-}
+export const SPOTLIGHT_OPEN_GOAL = "UHFB8XTI";
