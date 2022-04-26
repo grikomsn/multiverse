6678fa187aad37a047283274c02887d32c920512
Griko Nibras <git@griko.id>
2022-04-27 06:57:31 +0700

    feat(ui): add navigation route

--- website/ui/navigation/routes.ts
+++ website/ui/navigation/routes.ts
@@ -1,6 +1,7 @@
 export const routes = [
   { href: "/about", text: "About" },
   { href: "/work", text: "Work" },
+  { href: "/talk", text: "Talk" },
   { href: "/blog", text: "Blog" },
   { href: "/contact", text: "Contact" },
 ];
