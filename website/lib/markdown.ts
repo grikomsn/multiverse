9c53da8bcfd54441d1a9f27d74824ce03a3a06d1
Griko Nibras <git@griko.id>
2022-05-07 00:56:43 +0700

    feat(lib): improve prose heading link

--- website/lib/markdown.ts
+++ website/lib/markdown.ts
@@ -29,18 +29,39 @@ export const defaultMdxOptions: SerializeOptions["mdxOptions"] = {
       rehypeAutolinkHeadings,
       {
         content: (_node) => [
-          {
-            type: "element",
-            tagName: "span",
-            properties: { className: "absolute left-[-2ch]" },
-            children: [{ type: "text", value: "🔗" }],
-          },
+          { type: "element", tagName: "span", properties: { "data-heading-link": "" }, children: [linkSvgHast] },
         ],
       },
     ],
     [rehypePrettyCode, rehypePrettyCodeOptions],
   ],
   remarkPlugins: [remarkGfm],
+};
+
+const linkSvgHast = {
+  type: "element",
+  tagName: "svg",
+  properties: {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    stroke: "currentColor",
+    "stroke-width": 2,
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+  },
+  children: [
+    {
+      type: "element",
+      tagName: "path",
+      properties: { d: "M10 13a5 5 0 007.54.54l3-3a5 5 0 00-7.07-7.07l-1.72 1.71" },
+    },
+    {
+      type: "element",
+      tagName: "path",
+      properties: { d: "M14 11a5 5 0 00-7.54-.54l-3 3a5 5 0 007.07 7.07l1.71-1.71" },
+    },
+  ],
 };
 
 /* eslint-enable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
