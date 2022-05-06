2a8d819a8b47d4c28eacb6f0faf2477c576257cd
Griko Nibras <git@griko.id>
2022-05-07 01:22:46 +0700

    chore(ui): extract link icon hast

--- website/lib/markdown.ts
+++ website/lib/markdown.ts
@@ -6,6 +6,7 @@ import type { Options } from "rehype-pretty-code";
 import rehypePrettyCode from "rehype-pretty-code";
 import rehypeSlug from "rehype-slug";
 import remarkGfm from "remark-gfm";
+import { LinkIconHast } from "ui/icon/link";
 
 export const rehypePrettyCodeOptions: Partial<Options> = {
   onVisitHighlightedLine(node) {
@@ -29,39 +30,13 @@ export const defaultMdxOptions: SerializeOptions["mdxOptions"] = {
       rehypeAutolinkHeadings,
       {
         content: (_node) => [
-          { type: "element", tagName: "span", properties: { "data-heading-link": "" }, children: [linkSvgHast] },
+          { type: "element", tagName: "span", properties: { "data-heading-link": "" }, children: [LinkIconHast] },
         ],
       },
     ],
     [rehypePrettyCode, rehypePrettyCodeOptions],
   ],
   remarkPlugins: [remarkGfm],
-};
-
-const linkSvgHast = {
-  type: "element",
-  tagName: "svg",
-  properties: {
-    xmlns: "http://www.w3.org/2000/svg",
-    width: 24,
-    height: 24,
-    stroke: "currentColor",
-    "stroke-width": 2,
-    "stroke-linecap": "round",
-    "stroke-linejoin": "round",
-  },
-  children: [
-    {
-      type: "element",
-      tagName: "path",
-      properties: { d: "M10 13a5 5 0 007.54.54l3-3a5 5 0 00-7.07-7.07l-1.72 1.71" },
-    },
-    {
-      type: "element",
-      tagName: "path",
-      properties: { d: "M14 11a5 5 0 00-7.54-.54l-3 3a5 5 0 007.07 7.07l1.71-1.71" },
-    },
-  ],
 };
 
 /* eslint-enable @typescript-eslint/no-unsafe-call,@typescript-eslint/no-unsafe-member-access */
---
+++ website/ui/icon/link.tsx
@@ -0,0 +1,35 @@
+export function LinkIcon() {
+  return (
+    <svg {...LinkIconHast.properties}>
+      {LinkIconHast.children.map((node, i) => (
+        <path key={i} {...node.properties} />
+      ))}
+    </svg>
+  );
+}
+
+export const LinkIconHast = {
+  type: "element",
+  tagName: "svg",
+  properties: {
+    xmlns: "http://www.w3.org/2000/svg",
+    width: 24,
+    height: 24,
+    stroke: "currentColor",
+    "stroke-width": 2,
+    "stroke-linecap": "round",
+    "stroke-linejoin": "round",
+  },
+  children: [
+    {
+      type: "element",
+      tagName: "path",
+      properties: { d: "M10 13a5 5 0 007.54.54l3-3a5 5 0 00-7.07-7.07l-1.72 1.71" },
+    },
+    {
+      type: "element",
+      tagName: "path",
+      properties: { d: "M14 11a5 5 0 00-7.54-.54l-3 3a5 5 0 007.07 7.07l1.71-1.71" },
+    },
+  ],
+};
