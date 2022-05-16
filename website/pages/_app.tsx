ac5a5f3d284a2e943bbd3391ab13c0941245df3a
Griko Nibras <git@griko.id>
2022-05-17 05:10:54 +0700

    feat(pages): add axiom integration
    https://www.axiom.co/docs/integrations/vercel

--- website/pages/_app.tsx
+++ website/pages/_app.tsx
@@ -4,11 +4,23 @@ import "styles/index.css";
 
 import { TooltipProvider } from "@radix-ui/react-tooltip";
 import { queryClient } from "lib/react-query";
-import type { AppProps } from "next/app";
+import type { AppProps, NextWebVitalsMetric } from "next/app";
 import { QueryClientProvider } from "react-query";
 import { CentralProvider } from "store/central/provider";
 import { MarkdownProvider } from "store/markdown/provider";
 import { Layout } from "ui/layout";
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  const ENDPOINT = process.env.NEXT_PUBLIC_AXIOM_INGEST_ENDPOINT;
+  if (!ENDPOINT) return;
+
+  const body = JSON.stringify({ route: window.__NEXT_DATA__.page, ...metric });
+  if (navigator.sendBeacon) {
+    navigator.sendBeacon(ENDPOINT, body);
+  } else {
+    void fetch(ENDPOINT, { body, method: "POST", keepalive: true });
+  }
+}
 
 export default function CustomApp({ Component, pageProps }: AppProps) {
   return (
