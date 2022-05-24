f50827b304a87707f177026d225c98eabf7bf015
Griko Nibras <git@griko.id>
2022-05-24 15:45:40 +0700

    faet(ui): use hast typings for link icon

--- website/ui/icon/link.tsx
+++ website/ui/icon/link.tsx
@@ -1,3 +1,5 @@
+import { el } from "utils/markdown";
+
 export function LinkIcon() {
   return (
     <svg {...LinkIconHast.properties}>
@@ -8,7 +10,7 @@ export function LinkIcon() {
   );
 }
 
-export const LinkIconHast = {
+export const LinkIconHast = el({
   type: "element",
   tagName: "svg",
   properties: {
@@ -25,11 +27,13 @@ export const LinkIconHast = {
       type: "element",
       tagName: "path",
       properties: { d: "M10 13a5 5 0 007.54.54l3-3a5 5 0 00-7.07-7.07l-1.72 1.71" },
+      children: [],
     },
     {
       type: "element",
       tagName: "path",
       properties: { d: "M14 11a5 5 0 00-7.54-.54l-3 3a5 5 0 007.07 7.07l1.71-1.71" },
+      children: [],
     },
   ],
-};
+});
